2336f23c7c5d7c95b1837c3a0e48e0aaa587696f
Danyal Arif <danyal.arif.cowlar@gmail.com>
2024-02-15 17:54:13 +0500

    linter and prettier added in backend application

--- backend/mqtt.js
+++ backend/mqtt.js
@@ -3,26 +3,20 @@ const CITIES = ['Lahore', 'Islamabad', 'Karachi', 'Multan', 'Rawalpindi']
 import dotenv from 'dotenv'
 dotenv.config()
 const client = mqtt.connect(process.env.MQTT_URL);
-console.log(client)
-console.log(client)
 import Influx from './utilities/influx/index.js'
 const influx = global.influx
-
+console.dir(Influx)
 client.on("connect", async () => {
   //verify that database exists
   try {
     const databases = await influx.getDatabaseNames()
-    console.log('dbs' + databases)
     if (!databases.includes('weatherdb'))
       await influx.createDatabase('weatherdb')
   } catch(e) {
-    console.log('errq')
-    console.log(e)
     process.exit(1)
   }
   client.subscribe(process.env.MQTT_TOPIC, (err) => {
     if (err) {
-        console.log('Mqtt client unable to connect!', err)
         process.exit(1)
     }
   });
@@ -39,7 +33,6 @@ client.on("message", async (topic, message) => {
         fields: { temperature: message.temperature, humidity: message.humidity },
       }])
   } catch(e) {
-    console.log(e)
     process.exit(1)
   }
 });
--- backend/utilities/influx/index.js
+++ backend/utilities/influx/index.js
@@ -2,8 +2,6 @@ import Influx from 'influx'
 import dotenv from 'dotenv'
 dotenv.config()
 
-console.log(process.env.INFLUX_URL)
-console.log(process.env.INFLUX_PORT)
 const influx = new Influx.InfluxDB({
     host: process.env.INFLUX_URL,
     database: 'weatherdb',
@@ -22,7 +20,5 @@ const influx = new Influx.InfluxDB({
       }
     ]
   })
-console.log('influx')
-console.log(influx)
 global.influx = influx
 export default influx

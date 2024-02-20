49fa728baff82cf573ef01082b29d3711f53b8ce
Danyal Arif <danyal.arif.cowlar@gmail.com>
2024-02-20 20:33:08 +0500

    update

--- frontend/src/helpers/mqtt.js
+++ frontend/src/helpers/mqtt.js
@@ -1,30 +1,23 @@
-import mqtt from 'mqtt'
-console.log(process.env.VUE_APP_MQTT_URL)
-const client = mqtt.connect(process.env.VUE_APP_MQTT_URL);
-// eslint-disable-next-line no-unused-vars
-client.on("connect", () => {
-    console.log('connected')
-  client.subscribe(process.env.VUE_APP_MQTT_TOPIC, (err) => {
-    if (err) {
-        process.exit(1)
-    }
-  });
-});
-
-// client.on("message", async (topic, message) => {
-//   message = JSON.parse(message.toString())
-//   const messages = localStorage.getItem('messages') ? JSON.parse(localStorage.getItem('messages')) : []
-//   messages.push(message)
-//   localStorage.setItem('messages', JSON.stringify(messages))
+// import mqtt from 'mqtt'
+// console.log(process.env.VUE_APP_MQTT_URL)
+// const client = mqtt.connect(process.env.VUE_APP_MQTT_URL);
+// // eslint-disable-next-line no-unused-vars
+// client.on("connect", () => {
+//     console.log('connected')
+//   client.subscribe(process.env.VUE_APP_MQTT_TOPIC, (err) => {
+//     if (err) {
+//         process.exit(1)
+//     }
+//   });
 // });
 
-client.on('error', (err) => {
-    console.log(err)
-})
-
-// const closeMqttConnection = () => {
-//   client.end(); // This will close the connection to the MQTT broker
-// };
 
 
-export default client
+// client.on('error', (err) => {
+//     console.log(err)
+// })
+
+
+
+
+// export default client
